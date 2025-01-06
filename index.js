3c227172862eb77e5d612aa85c935f719c065dd8
yongcheol89 <yongcheol8912@gmail.com>
2025-01-06 11:16:29 +0900

    index

---
+++ index.js
@@ -0,0 +1,20 @@
+// 항목 1번 클릭 시 오른쪽 화면에 내용 업데이트
+document.getElementById('item1').addEventListener('click', function() {
+    document.getElementById('rightContent').innerHTML = '항목 1을 클릭했습니다!';
+  });
+  
+  // 항목 2번 클릭 시 오른쪽 화면에 내용 업데이트
+  document.getElementById('item2').addEventListener('click', function() {
+    document.getElementById('rightContent').innerHTML = '항목 2를 클릭했습니다!';
+  });
+  
+  // 항목 3번 클릭 시 오른쪽 화면에 내용 업데이트
+  document.getElementById('item3').addEventListener('click', function() {
+    document.getElementById('rightContent').innerHTML = '항목 3을 클릭했습니다!';
+  });
+  
+  // 항목 4번 클릭 시 오른쪽 화면에 내용 업데이트
+  document.getElementById('item4').addEventListener('click', function() {
+    document.getElementById('rightContent').innerHTML = '항목 4를 클릭했습니다!';
+  });
+  
